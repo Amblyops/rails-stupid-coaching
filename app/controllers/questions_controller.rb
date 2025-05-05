422ebae087cc07e29ab3da34ae1c795b70cc1a90
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-05 15:28:17 +0100

    returns correct instance variables

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+    def ask
+    end
+
+
+    #If the message is I am going to work, the coach will answer Great!
+    #If the message has a question mark ? at the end, the coach will answer Silly question,
+    #get dressed and go to work!.
+    #Otherwise the coach will answer I don't care, get dressed and go to work!
+
+
+
+    def answer
+      @question = params[:question]
+      case @question
+      when "I am going to work"
+        @answer = "Great!"
+      when "#{/\w*[?]/}"
+        @answer = "Silly question, get dressed and go to work!."
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    #raise
+    end
+end
